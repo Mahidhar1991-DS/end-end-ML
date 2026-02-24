8c4fe9397f0199bf0b8f420924ceaec9e210e881
Maheedhar Reddy <maheedhar3105@gmail.com>
2026-02-24 21:06:55 +0100

    Logging structure- second change

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ def get_requirements(file_path:str)->list[str]:
     
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-    requirements = [req.replace('\n','') for req in requirements]
-    if '-e .' in requirements:
-        requirements.remove('-e .')
+        requirements = [req.replace('\n','') for req in requirements]
+        if '-e .' in requirements:
+         requirements.remove('-e .')
     return requirements
 
 setup(
